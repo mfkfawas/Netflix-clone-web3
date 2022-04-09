3c770b1aa34f56aba7bd2f947b309f34b53bce89
mfkfawas <mfkfawas@gmail.com>
2022-04-10 01:03:25 +0530

    navbar & movies added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import "./Home.css";
+import { Link } from 'react-router-dom';
+import { ConnectButton, Icon, Tab, TabList } from 'web3uikit';
+
+import './Home.css';
+import { Logo } from '../images/Netflix';
 
 const Home = () => {
+  return (
+    <>
+      <div className='logo'>
+        <Logo />
+      </div>
 
-return(
-  <>
-  <div className="container">
-     <Link to="/player" className="link">Player Page</Link>
-  </div>
-  </>
-)
-}
+      <div className='connect'>
+        <Icon fill='#ffffff' size={24} svg='bell' />
+        <ConnectButton />
+      </div>
+
+      <div className='topBanner'>
+        <TabList defaultActiveKey={1} tabStyle='bar'>
+          <Tab tabKey={1} tabName={'Movies'}></Tab>
+          <Tab tabKey={2} tabName={'Series'} isDisabled={true}></Tab>
+          <Tab tabKey={3} tabName={'MyList'}></Tab>
+        </TabList>
+      </div>
+    </>
+  );
+};
 
 export default Home;
